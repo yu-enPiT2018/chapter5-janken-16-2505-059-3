bebc5d8697e2079abcbd0d264f6194a2ad9a7787
w059ff <w059ff@yamaguchi-u.ac.jp>
2018-07-13 15:23:16 +0900

    課題提出

--- app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.enpit_p12.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import com.example.enpit_p12.myapplication.R.drawable.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapHere.setOnClickListener {
+            textView.text = "ボタンがタップされました"
+        }
+
+        gu.setOnClickListener {onJankenButtonTapped(it)}
+        choki.setOnClickListener {onJankenButtonTapped(it)}
+        pa.setOnclickListener {onJankenButtonTapped(it)}
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND",view?.id)
+        startActivity(intent)
     }
 }
